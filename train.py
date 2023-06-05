90a0f9777a5a04e72678b6c5ae2f0576881729fe
motomura-s <105624274+motomura-s@users.noreply.github.com>
2023-06-05 19:51:11 +0900

    Update train.py

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ from transformers import (
     AutoModelForMaskedLM,
     AutoModelForSequenceClassification,
     AutoTokenizer,
+    BertJapaneseTokenizer,
     DataCollatorForLanguageModeling,
     DataCollatorWithPadding,
     HfArgumentParser,
@@ -338,9 +339,9 @@ def main():
         "use_auth_token": True if model_args.use_auth_token else None,
     }
     if model_args.tokenizer_name:
-        tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, **tokenizer_kwargs)
+        tokenizer = BertJapaneseTokenizer.from_pretrained(model_args.tokenizer_name, **tokenizer_kwargs)
     elif model_args.model_name_or_path:
-        tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, **tokenizer_kwargs)
+        tokenizer = BertJapaneseTokenizer.from_pretrained(model_args.model_name_or_path, **tokenizer_kwargs)
     else:
         raise ValueError(
             "You are instantiating a new tokenizer from scratch. This is not supported by this script."
